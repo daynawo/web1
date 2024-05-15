ecced6e6622e570d2a50cefd05d02558f44ff9ad
daynawo <daynawhitmanorlin@gmail.com>
2024-05-14 22:14:08 -0400

    fixed toggle menu

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var icon = document.getElementById("icon");
+  var list = document.getElementById("list");
+  if (icon) {
+    icon.addEventListener("click", () => {
+      if(!list.classList.contains("animate"))
+        {
+            icon.style.transform = "rotate(45deg)";
+            list.classList.add("animate");
+        }
+        else{
+            icon.style.transform = "rotate(0deg)";
+            list.classList.remove("animate");
+        }
+    });
+  }
+});
+
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+  myFunction()
+};
 
 // Get the header
 var header = document.getElementById("myHeader");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+if (header) {
+  var sticky = header.offsetTop;
+}
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
- if (window.pageYOffset > sticky) {
-   header.classList.add("sticky");
- } else {
-   header.classList.remove("sticky");
- }
+  if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+  } else {
+      header.classList.remove("sticky");
+  }
 }
+
+console.log("poop");
