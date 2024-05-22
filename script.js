cd532a5148a92c1e5366fc5a1242e989c7fc68f9
daynawo <daynawhitmanorlin@gmail.com>
2024-05-21 21:32:57 -0400

    stuff

--- script.js
+++ script.js
@@ -17,25 +17,11 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {
-  myFunction()
-};
-
-// Get the header
-var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-if (header) {
-  var sticky = header.offsetTop;
-}
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('.sticky-header');
+  if (window.scrollY > 0) {
+      header.classList.add('scrolled');
   } else {
-      header.classList.remove("sticky");
+      header.classList.remove('scrolled');
   }
-}
-
-console.log("poop");
+});
